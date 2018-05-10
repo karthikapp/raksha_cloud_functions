7ac234df25aec5684bc95ce7025794c2b11fae5e
karthikapp <karthik.uic@gmail.com>
2018-05-10 16:31:16 +0530

    qualifiedrest

--- index.js
+++ index.js
@@ -18,12 +18,8 @@ var object = require('lodash/fp/object');
 var at = require('lodash/at');
 var curryN = require('lodash/fp/curryN');
 
-
-
-
 admin.initializeApp();
 var db = admin.database();
-
 
 var transporter = nodemailer.createTransport(smtpTransport({
 	service: 'gmail',
@@ -36,42 +32,70 @@ var transporter = nodemailer.createTransport(smtpTransport({
 
 exports.sendMailto = functions.database.ref('/leads/{leadId}/leadstatus')
 .onUpdate((change,context) => {
-
 	var leadID = context.params.leadId
 	var leadsref = '/leads' + '/' + String(leadID) 
 	var original = change.before.val();
 	var new_value = change.after.val();
 	var leaddb = db.ref(leadsref)
 	var presalesheadids = []
-	leaddb.on("value",(event) => 
-	{
-		var leaddeatails = event.val()
-		console.log("lead", leaddeatails)
+	leaddb.on("value",(event) => {
+		var leaddetails = event.val();
+		//console.log("lead", leaddetails)
 
-		db.ref('/user').orderByChild('role').equalTo('presaleshead')
-		.once('value').then(snapshot => {
-			console.log("snapshot value" , snapshot.val())
-			var objectKeysArray = Object.keys(snapshot.val())
-			objectKeysArray.forEach(function(objKey) {
-				var objValue = snapshot.val()[objKey]
-				presalesheadids.push(objValue.email)
-				console.log('email',objValue.email)
-			})
+		var new_assignedto = leaddetails.assigned_to;
+		var new_reportsto = leaddetails.reports_to;
+		//console.log("1", new_assignedto, new_reportsto);
 
-		}).then(val => 
-		{
-			if (new_value == 'Qualified-awaiting-presales')
-			{   var test = ['karthik@zingr.org']
+		if (new_value == 'Qualified-awaiting-presales')
+		{ 
+			db.ref('/user').orderByChild('role').equalTo('presaleshead')
+			.once('value').then(snapshot => {
+				//console.log("snapshot value" , snapshot.val())
+				var objectKeysArray = Object.keys(snapshot.val())
+				objectKeysArray.forEach(function(objKey) {
+					var objValue = snapshot.val()[objKey]
+					presalesheadids.push(objValue.email)
+					//console.log('email',objValue.email)
+				})
+			}).then(val => {
+				var test = ['karthik@zingr.org']
 				presalesheadids = presalesheadids.concat(test)
-				console.log("emails now nj", presalesheadids.join())
+				//console.log("emails now nj", presalesheadids.join())
 				var uniqemails = _.uniq(presalesheadids)
 				var emails = uniqemails.join()
 				const mailOptions = {
-				  from: 'karthik.uic@gmail.com', // sender address
-				  to: emails, // list of receivers
-				  subject: 'A new lead has been qualified - Test', // Subject line
-				  text: "Hi, You have a new lead has been qualified.",
-				  html: "<h4> New Lead Qualified </h4>" 
+					from: 'karthik.uic@gmail.com', // sender address
+					to: emails, // list of receivers
+					subject: 'A new lead has been qualified - Test', // Subject line
+					text: "Hi, You have a new lead which has been qualified.",
+					html: "<h4> New Lead Qualified </h4>" 
+				};
+
+				transporter.sendMail(mailOptions,  (err, info) => {
+					if(err)
+						console.log("error from sendgrid",err)
+					else
+						console.log("sent by new ",info);
+					});
+
+				presalesheadids = []
+
+			})
+
+		}
+		else if(new_value == 'Qualified')
+		{
+			var assgnto = '/user/' + new_assignedto + '/email'
+			db.ref(assgnto)
+			.once('value', (snapshot) => {
+				var new_email_assgn = snapshot.val();
+				//console.log("2", new_email_assgn)
+				const mailOptions = {
+					from: 'karthik.uic@gmail.com', // sender address
+					to: new_email_assgn, // list of receivers
+					subject: 'A new lead has been qualified - Test', // Subject line
+					text: "Hi, You have a new lead which has been qualified.",
+					html: "<h4> New Lead Qualified </h4>" 
 				};
 
 				transporter.sendMail(mailOptions,  (err, info) => {
@@ -80,20 +104,38 @@ exports.sendMailto = functions.database.ref('/leads/{leadId}/leadstatus')
 					else
 						console.log("sent by new ",info);
 				});
+			})
+		}
+		else if(new_value == 'Qualified-awaiting-manager')
+		{
+			var repoto = '/user/' + new_reportsto + '/email';
+			db.ref(repoto)
+			.once('value', (snapshot) => {
+				var new_email_repo = snapshot.val();
+				//console.log("3", new_email_repo)
+				const mailOptions = {
+					from: 'karthik.uic@gmail.com', // sender address
+					to: new_email_repo, // list of receivers
+					subject: 'A new lead has been qualified - Test', // Subject line
+					text: "Hi, You have a new lead which has been qualified.",
+					html: "<h4> New Lead Qualified </h4>" 
+				};
 
-				presalesheadids = []
-
-			}
-
+				transporter.sendMail(mailOptions,  (err, info) => {
+					if(err)
+						console.log("error from sendgrid",err)
+					else
+						console.log("sent by new ",info);
+				});
+			})
+		}
+		else
+		{
+			return '';
 		}
 		
-		)
-		
 	})
-	
-		
-	}
 
-
-	)
+	return 'Completed'
+})
 		
